b7b46096a1e8808ea2458b917baf3cd94c7a1cc6
rachel-burns <109243988+rachel-burns@users.noreply.github.com>
2023-07-13 21:57:46 -0400

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Submission to "Natural Language Processing with Disaster Tweets" Kaggle competition',
+    author='Natalie Brooks & Rachel Burns',
+    license='',
+)
