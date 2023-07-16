ac981c879efd33b449efb62d3c4f2d01cf1a0e5a
rachel-burns <109243988+rachel-burns@users.noreply.github.com>
2023-07-16 10:54:00 -0400

    added word2vec embedding with random forest

--- src/data.py
+++ src/data.py
@@ -1,5 +1,31 @@
 #data import and preparation
 import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 def get_data(filepath):
     return pd.read_csv(filepath)
+
+#add keyword and location to doc2vec model
+def clean_docs(docs):
+    #real tokenization
+    docs = [x.lower().split() for x in docs]
+    #pre-process text (punctuation, emoji, tagging, links, etc)
+    return docs
+
+def get_tagged_docs(df):
+    docs = list(df['text'])
+    docs_clean = clean_docs(docs)
+    tagged_docs = [TaggedDocument(docs_clean[i], [i]) for i, doc in enumerate(docs_clean)]
+    return tagged_docs
+
+def get_vectors(tagged_docs, fname):
+    model = Doc2Vec.load('../models/'+fname)
+    doc_vectors = np.array([model.infer_vector(tagged_docs[i].words) for i in range(len(tagged_docs))])
+    return doc_vectors
+
+def preprocess_documents(df, fname='doc2vec.model'):
+    tagged_docs = get_tagged_docs(df)
+    doc_vectors = get_vectors(tagged_docs, fname)
+    return doc_vectors
--- src/models.py
+++ src/models.py
@@ -1,1 +1,9 @@
 # train and predict (break into two separate modules?)
+import data
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+
+def train_doc2vec(df, fname='doc2vec.model',vector_size=5, dm=1, window=2, min_count=1, workers=4, epochs=10):
+    tagged_docs = data.get_tagged_docs(df)
+    model_doc2vec = Doc2Vec(tagged_docs, vector_size=vector_size, dm=dm, window=window,  min_count=min_count, workers=workers, epochs=epochs)
+    model_doc2vec.save('../models/'+fname)
+    return model_doc2vec
